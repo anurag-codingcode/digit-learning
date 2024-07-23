b3ba9af6d689a03760b23763a0ea92b2b403588e
Anurag Omprakash <Anurag.Omprakash@godigit.com>
2024-07-23 07:23:29 +0530

    uc3 add presence of part time employee

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ if(attendance==1){
     console.log("absent")
 }
 
-let noOfHours=Math.floor(Math.random()*8)
-let wage=noOfHours*20
-console.log(wage)
+partOrFull=Math.floor(Math.random()*2)// for part is 0 and for full its 1
+if(partOrFull==0){
+    let noOfHour=4
+    console.log("part time employee wage for today is ",attendance*noOfHour*20)
+}else{
+
+    let noOfHour=8
+    console.log("full time employee wage for today is ",attendance*noOfHour*20)
+}
