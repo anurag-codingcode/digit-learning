bde8c1a8a4f46b29924ebe763046044dfddd107a
Anurag Omprakash <Anurag.Omprakash@godigit.com>
2024-07-23 07:45:00 +0530

    uc4 used switch case statement

--- index.js
+++ index.js
@@ -5,13 +5,21 @@ if(attendance==1){
 }else{
     console.log("absent")
 }
-
-partOrFull=Math.floor(Math.random()*2)// for part is 0 and for full its 1
+let noOfHourPerDay
+let partOrFull=Math.floor(Math.random()*2)// for part is 0 and for full its 1
 if(partOrFull==0){
-    let noOfHour=4
-    console.log("part time employee wage for today is ",attendance*noOfHour*20)
+    noOfHourPerDay=4
+    console.log("part time employee wage for today is ",attendance*noOfHourPerDay*20)
 }else{
 
-    let noOfHour=8
-    console.log("full time employee wage for today is ",attendance*noOfHour*20)
+    noOfHourPerDay=8
+    console.log("full time employee wage for today is ",attendance*noOfHourPerDay*20)
+}
+
+switch(partOrFull){
+    case 0:
+        console.log("part time employee wage for today is ",attendance*4*20)
+    case 1:
+        console.log("full time employee wage for today is ",attendance*8*20,"using switch")
+
 }
