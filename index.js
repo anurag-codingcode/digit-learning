8454addc1b4e08e2db2f18861b69e5d458ecbc49
Anurag Omprakash <Anurag.Omprakash@godigit.com>
2024-07-23 10:16:26 +0530

    uc7 implemented class

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 
-let wageForMonth=0
-let daysWorked=0
-let hoursWorked=0
-let partTimeStatus=Math.floor(Math.random()*2)
-for(let i=0;i<20;i++){
-    const attendance=Math.floor(Math.random()*2)
-    let working_hour
-    if (partTimeStatus==1){
-        working_hour=Math.floor(Math.random()*5)
-    }
-    else{
-        working_hour=Math.floor(Math.random()*9)
-    }
-    
-    if (attendance==1){
-        daysWorked+=1
-        hoursWorked+=working_hour
-        wageForMonth+=(attendance*working_hour*20)
+
+class WageCalculator {
+    static wageForMonth = 0
+    static daysWorked = 0
+    static hoursWorked = 0
+    static partTimeStatus = Math.floor(Math.random() * 2)
+    static WageForMonth(){
+        for(let i = 0; i<20; i++) {
+            const attendance = Math.floor(Math.random() * 2)
+            let working_hour
+            if (this.partTimeStatus == 1) {
+                working_hour = Math.floor(Math.random() * 5)
+            }
+            else {
+                working_hour = Math.floor(Math.random() * 9)
+            }
+        
+            if (attendance == 1) {
+                this.daysWorked += 1
+                this.hoursWorked += working_hour
+                this.wageForMonth += (attendance * working_hour * 20)
+            }
+        }
+        console.log(this.hoursWorked)
+            console.log(this.wageForMonth)
+            console.log(this.daysWorked)
+        
+
     }
 }
-console.log("days worked",daysWorked)
-console.log("wage for month",wageForMonth)
-console.log("hours worked ",hoursWorked)
+WageCalculator.WageForMonth()
+    
+
 
 
 
